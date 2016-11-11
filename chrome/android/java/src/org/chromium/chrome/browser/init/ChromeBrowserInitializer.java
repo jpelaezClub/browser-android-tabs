7d670ead7356b16580d2fa1e11041fb3729f4234
Serg <serg.zhukovsky@gmail.com>
2016-11-10 21:24:50 -0500

    Added stats calculation

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -75,6 +75,7 @@ public class ChromeBrowserInitializer {
     private boolean mNetworkChangeNotifierInitializationComplete;
 
     private boolean mAdBlockInitCalled = false;
+    private boolean mUpdateStatsCalled = false;
 
     /**
      * A callback to be executed when there is a new version available in Play Store.
@@ -123,6 +124,28 @@ public class ChromeBrowserInitializer {
       new DownloadHTTPSDataAsyncTask().execute();
       PrefServiceBridge.getInstance().setBlockThirdPartyCookiesEnabled(true);
       Log.i(TAG, "Started AdBlock async tasks");
+    }
+
+    private void UpdateStats() {
+      if (mUpdateStatsCalled) {
+          return;
+      }
+      mUpdateStatsCalled = true;
+      new UpdateStatsAsyncTask().execute();
+    }
+
+    // Stats update
+    class UpdateStatsAsyncTask extends AsyncTask<Void,Void,Long> {
+        protected Long doInBackground(Void... params) {
+            try {
+                StatsUpdater.UpdateStats(mContext);
+            }
+            catch(Exception exc) {
+                // Just ignore it if we cannot update
+            }
+
+            return null;
+        }
     }
 
     // Tracking ptotection data download
@@ -354,6 +377,7 @@ public class ChromeBrowserInitializer {
                     () -> {
                         ProcessInitializationHandler.getInstance().initializePostNative();
                         InitAdBlock();
+                        UpdateStats();
                     });
         }
 
