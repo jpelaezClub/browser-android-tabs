4e89d595b25a9fd4f18662bd4c4bdfe4d32267fc
Serg <serg.zhukovsky@gmail.com>
2017-01-16 18:43:29 -0500

    Disabled regonal adblock to not whitelisted locales by default
    Auditors: @bbondy

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -54,6 +54,7 @@ import org.chromium.policy.CombinedPolicyProvider;
 import org.chromium.ui.resources.ResourceExtractor;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -77,6 +78,8 @@ public class ChromeBrowserInitializer {
 
     private boolean mAdBlockInitCalled = false;
     private boolean mUpdateStatsCalled = false;
+
+    List<String> mWhitelistedRegionalLocales = Arrays.asList("ru", "uk", "be", "hi");
 
     /**
      * A callback to be executed when there is a new version available in Play Store.
@@ -191,8 +194,9 @@ public class ChromeBrowserInitializer {
         protected Long doInBackground(Void... params) {
             String verNumber = ADBlockUtils.getDataVerNumber(
                 ADBlockUtils.ADBLOCK_REGIONAL_URL, true);
+            final String deviceLanguage = Locale.getDefault().getLanguage();
             List<String> files = ADBlockUtils.readRegionalABData(ContextUtils.getApplicationContext(),
-                ADBlockUtils.ETAG_PREPEND_REGIONAL_ADBLOCK, verNumber);
+                ADBlockUtils.ETAG_PREPEND_REGIONAL_ADBLOCK, verNumber, deviceLanguage);
             if (null == files) {
                 return null;
             }
@@ -210,8 +214,13 @@ public class ChromeBrowserInitializer {
                 mHandler.post(new Runnable() {
                     @Override
                     public void run() {
-                        PrivacyPreferencesManager.getInstance().setRegionalAdBlock(enableRegionalAdBlock);
-                        PrefServiceBridge.getInstance().setAdBlockRegionalEnabled(enableRegionalAdBlock);
+                        if (enableRegionalAdBlock && !mWhitelistedRegionalLocales.contains(deviceLanguage)) {
+                            PrivacyPreferencesManager.getInstance().setRegionalAdBlock(false, false);
+                            PrefServiceBridge.getInstance().setAdBlockRegionalEnabled(false);
+                        } else {
+                            PrivacyPreferencesManager.getInstance().setRegionalAdBlock(enableRegionalAdBlock, true);
+                            PrefServiceBridge.getInstance().setAdBlockRegionalEnabled(enableRegionalAdBlock);
+                        }
                     }
                 });
             }
