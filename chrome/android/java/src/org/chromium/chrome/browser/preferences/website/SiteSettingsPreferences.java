4dc31ee7ed8bc5828a1f09a19cd1faf56269b952
AlexeyBarabash <alexey.barabash@gmail.com>
2017-09-26 12:52:42 +0300

    Disabled Desktop View option until issue isn't fixed

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsPreferences.java
@@ -37,6 +37,7 @@ public class SiteSettingsPreferences
     // are defined in the SiteSettingsCategory, additional keys
     // are listed here.
     static final String MEDIA_KEY = "media";
+    //static final String DESKTOP_VIEW_SETTINGS_KEY = "desktop_view";
 
     // Whether this class is handling showing the Media sub-menu (and not the main menu).
     boolean mMediaSubMenu;
@@ -78,6 +79,7 @@ public class SiteSettingsPreferences
                 getPreferenceScreen().removePreference(findPreference(i));
             }
             getPreferenceScreen().removePreference(findPreference(MEDIA_KEY));
+            //getPreferenceScreen().removePreference(findPreference(DESKTOP_VIEW_SETTINGS_KEY));
         } else {
             // These will be tucked under the Media subkey, so don't show them on the main menu.
             getPreferenceScreen().removePreference(findPreference(Type.AUTOPLAY));
@@ -133,7 +135,7 @@ public class SiteSettingsPreferences
             if (ChromeFeatureList.isEnabled(ChromeFeatureList.SENSOR_CONTENT_SETTING)) {
                 websitePrefs.add(Type.SENSORS);
             }
-            websitePrefs.add(Type.DESKTOP_VIEW_SETTINGS);
+            //websitePrefs.add(Type.DESKTOP_VIEW_SETTINGS);
             websitePrefs.add(Type.SOUND);
             websitePrefs.add(Type.USB);
         }
@@ -181,9 +183,9 @@ public class SiteSettingsPreferences
                 p.setSummary(ContentSettingsResources.getSoundBlockedListSummary());
             } else if (requiresTriStateSetting) {
                 p.setSummary(ContentSettingsResources.getCategorySummary(setting));
-            } else if (SiteSettingsCategory.Type.DESKTOP_VIEW_SETTINGS == prefCategory) {
+            }/* else if (SiteSettingsCategory.Type.DESKTOP_VIEW_SETTINGS == prefCategory) {
                 p.setSummary( checked ? ContentSettingsResources.getDesktopViewEnabledSummary() : ContentSettingsResources.getDesktopViewDisabledSummary());
-            } else {
+            }*/ else {
                 p.setSummary(ContentSettingsResources.getCategorySummary(contentType, checked));
             }
 
