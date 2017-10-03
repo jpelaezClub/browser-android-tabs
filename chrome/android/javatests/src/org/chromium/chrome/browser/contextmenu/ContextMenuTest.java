fbcb1adb082c6c8d2cf2caa0571fc5c859f0e0c8
samartnik <artem@brave.com>
2017-10-03 15:18:23 +0300

    Changes for running Instrumentation Tests

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -358,7 +358,7 @@ public class ContextMenuTest implements CustomMainActivityStart {
 
     @Test
     @SmallTest
-    @Feature({"Browser", "ContextMenu"})
+    @Feature({"Browser", "ContextMenu", "ApplyLater"})
     @RetryOnFailure
     public void testContextMenuRetrievesLinkOptions()
             throws TimeoutException, InterruptedException {
