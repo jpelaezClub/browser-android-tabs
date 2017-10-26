1432e0be163c8f4c1e195566d87b18eec30048bf
samartnik <artem@brave.com>
2017-10-26 14:47:58 +0300

    Fixes or build after rebase

--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -220,7 +220,7 @@ void AddAdditionalRequestHeaders(net::HttpRequestHeaders* headers,
   headers->SetHeaderIfMissing(
       net::HttpRequestHeaders::kUserAgent,
       user_agent_override.empty()
-          ? GetContentClient()->browser()->GetUserAgent()
+          ? GetContentClient()->browser()->GetUserAgent("")
           : user_agent_override);
 
   // TODO(mkwst): Extract this logic out somewhere that can be shared between
@@ -286,13 +286,8 @@ void AddAdditionalRequestHeaders(net::HttpRequestHeaders* headers,
                                   : user_agent_override);
 
   // Check whether DevTools wants to override user agent for this request
-  // after setting the default user agent.
-  std::string devtools_user_agent =
-      RenderFrameDevToolsAgentHost::UserAgentOverride(frame_tree_node);
-  if (!devtools_user_agent.empty()) {
-    headers->SetHeader(net::HttpRequestHeaders::kUserAgent,
-                       devtools_user_agent);
-  }
+  // after setting the default user agent, or append throttling control header.
+  RenderFrameDevToolsAgentHost::AppendDevToolsHeaders(frame_tree_node, headers);
 }
 
 // Should match the definition of
