636068c7c88450d997597a3181a364a76c238c71
AlexeyBarabash <alexey@brave.com>
2018-01-12 19:28:07 +0200

    option 'Play YouTube video in Brave'

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
@@ -496,6 +496,8 @@ public class SingleCategoryPreferences extends PreferenceFragmentCompat
             } else if (mCategory.showPlayVideoInBackground()) {
                 PrefServiceBridge.getInstance().setPlayVideoInBackgroundEnabled((boolean) newValue);
                 AskForRelaunch();
+            } else if (mCategory.showPlayYTVideoInBrowser()) {
+                PrefServiceBridge.getInstance().setPlayYTVideoInBrowserEnabled((boolean) newValue);
             }
 
             // Categories that support adding exceptions also manage the 'Add site' preference.
@@ -1002,6 +1004,8 @@ public class SingleCategoryPreferences extends PreferenceFragmentCompat
                 binaryToggle.setChecked(PrefServiceBridge.getInstance().desktopViewEnabled());
             } else if (mCategory.showPlayVideoInBackground()) {
                 binaryToggle.setChecked(PrefServiceBridge.getInstance().playVideoInBackgroundEnabled());
+            } else if (mCategory.showPlayYTVideoInBrowser()) {
+                binaryToggle.setChecked(PrefServiceBridge.getInstance().playYTVideoInBrowserEnabled());
             }
         }
     }
