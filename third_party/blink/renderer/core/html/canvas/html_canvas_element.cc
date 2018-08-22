0208e93d51c5ba5b567ef9a2790997a2cdf77cb2
samartnik <artem@brave.com>
2018-08-22 18:54:23 +0300

    Fixed canvas fingerprinting protection behavior

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -1027,6 +1027,9 @@ bool HTMLCanvasElement::OriginClean() const {
   if (frame) {
       allowed = frame->Loader().Client()->AllowFingerprinting();
   }
+  if (GetDocument().GetSettings()) {
+    GetDocument().GetSettings()->SetDisableReadingFromCanvas(!allowed);
+  }
   if (!allowed) {
       if (frame) {
         if (frame && !wasBlockedByFingerprinting_) {
