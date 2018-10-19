9c881d7189aa974629cc35afd8227b24d1a81b6f
samartnik <artem@brave.com>
2018-10-19 16:08:15 +0300

    Fixed build for cr-70

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
@@ -993,7 +993,6 @@ public class SingleCategoryPreferences extends PreferenceFragmentCompat
                     LocationSettings.getInstance().isChromeLocationSettingEnabled());
         } else {
             binaryToggle.setChecked(PrefServiceBridge.getInstance().isCategoryEnabled(contentType));
-            }
         }
     }
 
