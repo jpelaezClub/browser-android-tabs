38d7f8f2e1471e149cf5e4788771bb89cbefd3e4
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2018-10-25 15:02:19 -0400

    adding chrome://rewards-panel page

--- chrome/common/webui_url_constants.h
+++ chrome/common/webui_url_constants.h
@@ -170,11 +170,14 @@ extern const char kChromeUIVersionURL[];
 extern const char kChromeUIWelcomeHost[];
 extern const char kChromeUIWelcomeURL[];
 extern const char kRewardsJS[];
+extern const char kRewardsPanelJS[];
 
 #if defined(OS_ANDROID)
 extern const char kChromeUIExploreSitesInternalsHost[];
 extern const char kBraveRewardsURL[];
 extern const char kBraveRewardsHost[];
+extern const char kBraveRewardsPanelURL[];
+extern const char kBraveRewardsPanelHost[];
 extern const char kChromeUIJavaCrashURL[];
 extern const char kChromeUINativeBookmarksURL[];
 extern const char kChromeUINativeExploreURL[];
