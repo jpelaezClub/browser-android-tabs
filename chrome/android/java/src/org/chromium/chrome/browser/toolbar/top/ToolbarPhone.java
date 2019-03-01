106640dcb41b0e8e3e46d16e6e0d323c3a70f1b8
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-03-01 17:52:01 -0500

    Welcome back screen

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2069,6 +2069,10 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
     public void OnGetPendingContributionsTotal(double amount) {}
 
     @Override
+    public void OnGetRewardsMainEnabled(boolean enabled) {}
+
+
+    @Override
     public void setOnTabSwitcherClickHandler(OnClickListener listener) {
         if (mToggleTabStackButton != null) {
             mToggleTabStackButton.setOnTabSwitcherClickHandler(listener);
