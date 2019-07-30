56c2ca3be8e8e8fc66da6dde33f7ebc71f83e9a2
samartnik <artem@brave.com>
2019-07-30 09:44:29 -0400

    Removed TP code as it is already included in adblock library

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
@@ -41,7 +41,6 @@ public class PrivacyPreferences
     private static final String PREF_USAGE_STATS = "usage_stats_reporting";
     private static final String PREF_FINGERPRINTING_PROTECTION = "fingerprinting_protection";
     private static final String PREF_HTTPSE = "httpse";
-    private static final String PREF_TRACKING_PROTECTION = "tracking_protection";
     private static final String PREF_AD_BLOCK = "ad_block";
     private static final String PREF_DO_NOT_TRACK = "do_not_track";
     private static final String PREF_AD_BLOCK_REGIONAL = "ad_block_regional";
@@ -91,11 +90,6 @@ public class PrivacyPreferences
         httpsePref.setOnPreferenceChangeListener(this);
         httpsePref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
 
-        ChromeBaseCheckBoxPreference trackingProtectionPref =
-                (ChromeBaseCheckBoxPreference) findPreference(PREF_TRACKING_PROTECTION);
-        trackingProtectionPref.setOnPreferenceChangeListener(this);
-        trackingProtectionPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
-
         ChromeBaseCheckBoxPreference adBlockPref =
                 (ChromeBaseCheckBoxPreference) findPreference(PREF_AD_BLOCK);
         adBlockPref.setOnPreferenceChangeListener(this);
@@ -127,9 +121,6 @@ public class PrivacyPreferences
                     Pref.CAN_MAKE_PAYMENT_ENABLED, (boolean) newValue);
         } else if (PREF_NETWORK_PREDICTIONS.equals(key)) {
             PrefServiceBridge.getInstance().setNetworkPredictionEnabled((boolean) newValue);
-        } else if (PREF_TRACKING_PROTECTION.equals(key)) {
-            PrefServiceBridge.getInstance().setTrackingProtectionEnabled((boolean) newValue);
-            //MixPanelWorker.SendEvent("Tracking Protection Mode Option Changed", "Tracking Protection Mode", newValue);
         } else if (PREF_AD_BLOCK.equals(key)) {
             PrefServiceBridge.getInstance().setAdBlockEnabled((boolean) newValue);
         } else if (PREF_HTTPSE.equals(key)) {
