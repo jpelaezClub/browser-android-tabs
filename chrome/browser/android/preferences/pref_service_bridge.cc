remzert/BraveBrowser

Added regional adblock
Auditors: @bbondy

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -360,6 +360,12 @@ static void SetAdBlockEnabled(JNIEnv* env,
    GetPrefService()->SetBoolean(prefs::kAdBlockEnabled, enabled);
 }
 
+static void SetAdBlockRegionalEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kAdBlockRegionalEnabled, enabled);
+}
+
 static void SetTrackingProtectionEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {

jpelaezClub/browser-android-tabs

Conflicts:
	chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
	chrome/common/pref_names.cc
	chrome/common/pref_names.h

chrome/browser/android/preferences/pref_service_bridge.cc
@@ -464,6 +464,12 @@ static void JNI_PrefServiceBridge_SetAdBlockEnabled(JNIEnv* env,
    GetPrefService()->SetBoolean(prefs::kAdBlockEnabled, enabled);
 }
 
+static void JNI_PrefServiceBridge_SetAdBlockRegionalEnabled(JNIEnv* env,
+                                   const JavaParamRef<jobject>& obj,
+                                   jboolean enabled) {
+   GetPrefService()->SetBoolean(prefs::kAdBlockRegionalEnabled, enabled);
+}
+
 static void JNI_PrefServiceBridge_SetTrackingProtectionEnabled(JNIEnv* env,
                                    const JavaParamRef<jobject>& obj,
                                    jboolean enabled) {